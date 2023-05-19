7d537102b0c5c2ab298ae642d0b214eb43564b86
Franco Ojeda <fojeda@slingr.io>
2023-05-19 10:18:14 -0300

    service test

--- scripts/http.js
+++ scripts/http.js
@@ -1,6 +1,25 @@
 exports.serviceTest = function (){
-
     var response = svc.http.get({
+        url: "https://postman-echo.com/get",
+        params: {
+            queryVal: "1234"
+        },
+        headers: {
+            key: "123"
+        },
+        forceDownload: false,
+        downloadSync: false,
+        fileName: "",
+        fullResponse: false,
+        connectionTimeout: "5000",
+        readTimeout: "60000",
+        defaultCallback: false
+    });
+    return JSON.stringify(response);
+}
+
+exports.serviceTest2 = function (){
+    var response = dependencies.svc.http.get({
         url: "https://postman-echo.com/get",
         params: {
             queryVal: "1234"
