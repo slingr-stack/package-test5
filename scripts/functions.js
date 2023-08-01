f4e6c4ac4a30ea031db79f4fe79a3c65b210f886
juan <jguardiolagarcia@gmail.com>
2023-08-01 16:56:53 -0300

    event name

--- scripts/functions.js
+++ scripts/functions.js
@@ -1419,7 +1419,7 @@ var parse = function (str) {
  oauth
  ****************************************************/
 exports.getAccessToken = function () {
-    return dependencies.oauth.functions.connectUser('sharepoint:userConnected');
+    return dependencies.oauth.functions.connectUser('sharepoint-outh:userConnected');
 }
 
 function handleRequestWithRetry(requestFn, options, callbackData, callbacks) {
